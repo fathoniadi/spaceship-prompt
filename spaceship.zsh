2273ba863bb3de47f33b9b6deedf059b3515f301
fathoniadi <fathoni.adi@gmail.com>
2018-09-08 17:46:11 +0700

    oke

--- spaceship.zsh
+++ spaceship.zsh
@@ -41,6 +41,7 @@ fi
 if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
   SPACESHIP_PROMPT_ORDER=(
     time          # Time stampts section
+    battery	  # Battery
     user          # Username section
     dir           # Current directory section
     host          # Hostname section
@@ -67,7 +68,7 @@ if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
     kubecontext   # Kubectl context section
     exec_time     # Execution time
     line_sep      # Line break
-    battery       # Battery level and status
+    #battery       # Battery level and status
     vi_mode       # Vi-mode indicator
     jobs          # Background jobs indicator
     exit_code     # Exit code section
@@ -248,3 +249,43 @@ SPACESHIP_PYENV_SHOW="true"
 SPACESHIP_DIR_TRUNC="0"
 
 SPACESHIP_BATTERY_SHOW="always"
+
+SPACESHIP_BATTERY_THRESHOLD="20"
+
+SPACESHIP_USER_PREFIX="| "
+
+SPACESHIP_PROMPT_ORDER=(
+    time          # Time stampts section
+    battery       # Battery
+    user          # Username section
+    dir           # Current directory section
+    host          # Hostname section
+    git           # Git section (git_branch + git_status)
+    hg            # Mercurial section (hg_branch  + hg_status)
+    package       # Package version
+    node          # Node.js section
+    ruby          # Ruby section
+    elixir        # Elixir section
+    xcode         # Xcode section
+    swift         # Swift section
+    golang        # Go section
+    php           # PHP section
+    rust          # Rust section
+    haskell       # Haskell Stack section
+    julia         # Julia section
+    docker        # Docker section
+    aws           # Amazon Web Services section
+    venv          # virtualenv section
+    conda         # conda virtualenv section
+    pyenv         # Pyenv section
+    dotnet        # .NET section
+    ember         # Ember.js section
+    kubecontext   # Kubectl context section
+    exec_time     # Execution time
+    line_sep      # Line break
+    #battery       # Battery level and status
+    vi_mode       # Vi-mode indicator
+    jobs          # Background jobs indicator
+    exit_code     # Exit code section
+    char          # Prompt character
+  )
